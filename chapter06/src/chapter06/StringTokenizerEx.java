4134c8b1437ea6829c332281f92a2dd6208854aa
Antonio900 <gbw0115@naver.com>
2016-10-26 11:05:21 +0900

    add the Token's number

--- chapter06/src/chapter06/StringTokenizerEx.java
+++ chapter06/src/chapter06/StringTokenizerEx.java
@@ -4,6 +4,8 @@ public class StringTokenizerEx {
 
 	public static void main(String[] args) {
 				StringTokenizer st = new StringTokenizer("È«±æµ¿/ÀåÈ­/È«·Ã/ÄáÁã/ÆÏÁã","/");
+				int n = st.countTokens();
+				System.out.println(n);
 				
 				while(st.hasMoreTokens())
 					System.out.println(st.nextToken());
